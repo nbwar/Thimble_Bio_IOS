53c6f605e78a5435a6bcb9dea433ac312ef1d46c
Nicholas Wargnier <nwargnier@gmail.com>
2013-11-01 14:36:43 -0700

    storyboard and back button

---
+++ ThimbleBioGen/NWDeviceLocatorViewController.h
@@ -0,0 +1,15 @@
+//
+//  NWDeviceLocatorViewController.h
+//  ThimbleBioGen
+//
+//  Created by Nicholas Wargnier on 11/1/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWDeviceLocatorViewController : UITableViewController
+
+- (IBAction)backButtonPressed:(UIBarButtonItem *)sender;
+
+@end
