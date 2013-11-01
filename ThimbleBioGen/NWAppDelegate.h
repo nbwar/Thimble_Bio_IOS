0e77d918dd2f11ebb384906e71e362a15ecb8f21
Nicholas Wargnier <nwargnier@gmail.com>
2013-10-31 23:37:14 -0700

    Initial Commit

---
+++ ThimbleBioGen/NWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NWAppDelegate.h
+//  ThimbleBioGen
+//
+//  Created by Nicholas Wargnier on 10/31/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ThimbleBioGen/NWViewController.h
@@ -0,0 +1,13 @@
+//
+//  NWViewController.h
+//  ThimbleBioGen
+//
+//  Created by Nicholas Wargnier on 10/31/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWViewController : UIViewController
+
+@end
